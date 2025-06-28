4fa630ab994305043e48ae5a1b05d511fb2a8c07
Hafdala Mehdi Sidi <hafdalamehdisidi@gmail.com>
2025-06-28 23:31:05 +0200

    I have created the main entity with its constructors, getters and setters and its JPA annotations.

---
+++ backend/crud-fullstack-angular/crud-fullstack-angular/src/main/java/com/sidi/crudfullstack/crud_fullstack_angular/entity/Customer.java
@@ -0,0 +1,61 @@
+package com.sidi.crudfullstack.crud_fullstack_angular.entity;
+
+import jakarta.persistence.*;
+
+/**
+ * @author Hafdala Mehdi Sidi
+ */
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    // Constructor vacio
+    public Customer() {
+    }
+
+    public Customer(Integer id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
